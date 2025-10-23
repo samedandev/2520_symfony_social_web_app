c8e622181351f5276c8b7abe1ed4e11026ed54f8
John Doe <johndoe@example.com>
2025-10-23 17:56:03 +0200

    if/for in Twig templates

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -17,7 +17,7 @@ class HelloController extends AbstractController
    {
       return $this->render(
         'hello/index.html.twig',
-        ['message' => implode(', ', array_slice($this->messages, 0, $limit) )]
+        ['messages' => array_slice($this->messages, 0, $limit) ]
       );
     //  return new Response(
     //    implode(', ', array_slice($this->messages, 0, $limit) )); 
